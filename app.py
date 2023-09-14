4812661b15e646d75911bbde6fa95570d18ea2bd
jlo-dcjd <91139882+jlo-dcjd@users.noreply.github.com>
2023-09-14 11:34:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ st.markdown('#')
 uploaded_file = st.file_uploader("Upload your Excel file")
 if uploaded_file is not None:
      df = pd.read_excel(uploaded_file)
+     df = df.sort_values(by=df.columns[3])
 
      batch = pd.DataFrame()
 
